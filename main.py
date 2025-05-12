1cd917f9b64161280c53accf16c91f90c71ebfc9
Lazy Raccoon <counter088@gmail.com>
2025-05-12 21:09:27 +0300

    [Main] Modify main loop. Add input-based controls.

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-from mechanics import *
+from copy import deepcopy
+
+from mechanics import Grid, History
 
 
 def main():
-    # perhaps let player choose grid size: 4x4, 5x5, or 6x6
+    # perhaps players should choose grid size: 4x4, 5x5, or 6x6
     # for now it'll stay 4x4
     # Set size and build a grid
-    grid_size = 4
-    grid = build_grid(grid_size)
+    grid_size: int = 4
+    grid: Grid = Grid(grid_size)
+    prev_moves: History = History()
 
-    # Spawn 2 random elements on the grid
-    spawn_random_el(grid)
-    spawn_random_el(grid)
-
-
-    for row in grid:
-        print(row)
-
-    
+    while True:
+        # print the current grid state to CLI
+        print(grid, "="*20, sep="\n")
+        # movement controls for CLI
+        action = input("Your move: ")
+        if action in "wasd":
+            prev_moves.save_move(Grid(grid_size, deepcopy(grid.current)))
+            match action:
+                case "a":
+                    grid.move_left()
+                case "d":
+                    grid.move_right()
+                case "s":
+                    grid.move_down()
+                case "w":
+                    grid.move_up()
+                case _:
+                    break
+            if not prev_moves.compare(grid):
+                grid.spawn_random_el()
+        elif action == "u":
+            if prev_moves.length > 0:
+                    grid = prev_moves.undo_move()
+        else:
+            break
 
 
 if __name__ == "__main__":
